e0bf825c05ade1fd9f55359911070471b375d65d
root <root@inst-builder-debian-11-build-build-ly0tq.us-central1-a.c.gce-image-builder.internal>
2024-07-17 05:23:28 +0000

    Init

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,13 +8,14 @@ return {
   },
 
   -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
+  {
+     "neovim/nvim-lspconfig",
+     config = function()
+       require("nvchad.configs.lspconfig").defaults()
+       require "configs.lspconfig"
+     end,
+   },
+   {"github/copilot.vim" },
   --
   -- {
   -- 	"williamboman/mason.nvim",
